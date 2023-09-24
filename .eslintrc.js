3a5895872b0457e1c0c81555b11fa9cf70c1ce3d
erwin <erwinsetyoprasetyo@gmail.com>
2023-09-24 23:31:29 +0700

    consumer program finished

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'no-underscore-dangle': 'off',
+    'eol-last': 'off',
+  },
+};
